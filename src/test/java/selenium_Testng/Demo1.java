5c94bd375f73aea36df23f6a170515c622b2947a
Shrinand12B <shrinandbitla@gmail.com>
2024-03-22 00:10:04 +0530

    testng

---
+++ src/test/java/selenium_Testng/Demo1.java
@@ -0,0 +1,16 @@
+package selenium_Testng;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+	@Test
+	public void method1() {
+		
+		System.out.print("hello");
+	}
+	
+	public void method2() {
+		System.out.print("hello");
+	}
+}
